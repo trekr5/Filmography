7f7db90ea728f43176a7c14b00aa260e7d690ed3
Angela Ebirim & Helene Martin <helene.martin@fundingcircle.com>
2014-04-01 17:53:46 +0100

    changed files

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,9 +5,23 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.new(params[:review])
+    @review = Review.new(review_params)
     @review.save
     redirect_to @review
+  end
+
+  def show
+     @review = Review.find(params[:id])
+  end  
+
+  def index
+     @reviews = Review.all
+  end  
+
+  private
+
+  def review_params
+    params.require(:review).permit(:username, :name_of_film, :description, :comment, :ratings)
   end
 
 
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -5,14 +5,21 @@ describe ReviewsController do
     get :new
     response.should be_ok
   end
+  
 
+  it "has a show action" do 
+    @review = Review.create
+    get :show, id: @review.id
+    response.should be_ok
+  end
 
   describe "POST #create" do 
       it "saves a record" do
 
         expect {
-         post :create
+         post :create, review: {username: "Helene", name_of_film: "Capybara", description: "Gem movie", comment: "Dodgy", ratings: "Outstanding" }
          }.to change{ Review.count }.from(0).to(1) 
   end
+
   end
 end
