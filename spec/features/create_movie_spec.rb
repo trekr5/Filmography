d8edaa3613bf8401423f02ea4f1737edd632f20a
angela ebirim <angelaebirim@Jacks-MacBook-Air-2.local>
2014-04-02 14:18:09 +0100

    rspec test for new feature

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -4,11 +4,11 @@ feature "Movie" do
 
   scenario "seeing a list of movies" do 
   	  
-  	  brave_film = FactoryGirl.create(:movie, name: "Brave")
-  	  up_film = FactoryGirl.create(:movie, name: "UP")
+  	  brave_film = FactoryGirl.create(:movie, title: "Brave")
+  	  up_film = FactoryGirl.create(:movie, title: "UP")
       visit '/movies'
-      page.should have_content brave_review.name
-      page.should have_content up_review.name
+      page.should have_content brave_review.name_of_film
+      page.should have_content up_review.name_of_film
       
 
   end 
