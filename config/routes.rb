d6b98a91839993c4d5b72d22ea20924f9b45b5e5
Teja Kocjancic <teja.kocjancic@fundingcircle.com>
2014-04-09 11:04:19 +0100

    soemstyles

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Filmography::Application.routes.draw do
 
   resources :reviews 
   resources :movies
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
